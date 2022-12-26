8882c75a6c745f42da8239c031e6b70b96f5e86a
Roberto Caiola <robertocaiola.decskill@edp.pt>
2022-12-26 20:18:29 +0000

    Traefik - Add configuration

--- makefiles/server.mk
+++ makefiles/server.mk
@@ -2,13 +2,13 @@
 # ¯¯¯¯¯¯¯¯¯¯¯
 
 dev: ## Install server with its dependencies
-	docker-compose up --build apiserver nginx
+	docker-compose up --build traefik apiserver webserver app1
 
 server.install: ## Install server with its dependencies
 	docker-compose run --rm apiserver pip install -r requirements-dev.txt --user --upgrade --no-warn-script-location
 
 server.start: ## Start server in its docker container
-	docker-compose up apiserver
+	docker-compose up traefik apiserver webserver app1
 
 server.bash: ## Connect to server to lauch commands
 	docker-compose exec apiserver bash
@@ -24,3 +24,6 @@ server.logs: ## Display server logs
 
 server.upgrade: ## Upgrade pip dependencies
 	docker-compose run --rm apiserver bash -c "python vendor/bin/pip-upgrade requirements.txt requirements-dev.txt --skip-virtualenv-check"
+
+server.network: ## Create network netpublic
+	docker network create netpublic
