7595d1f13a6370d6fb643c921cee1cb114de3175
Sérgio den Boer <den.boer.sergio@gmail.com>
2023-01-22 11:36:26 +0000

    Correct 'Adds' typo. Model is 'Ad'

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -3,4 +3,4 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .users import User
-from .adds import Add
+from .ads import Ad
--- backend/api/models/ads.py
+++ backend/api/models/ads.py
@@ -5,19 +5,19 @@ from . import db
 from .abc import BaseModel, MetaBaseModel
 import uuid
 
-class Add(db.Model, BaseModel, metaclass=MetaBaseModel):
-    """ The User model """
+class Ad(db.Model, BaseModel, metaclass=MetaBaseModel):
+    """ The Ad model """
 
-    __tablename__ = "adds"
+    __tablename__ = "ads"
 
-    pk = db.Column(db.BigInteger(), primary_key=True)
+    id = db.Column(db.BigInteger(), primary_key=True)
     uuid = db.Column(db.String(32), default=uuid.uuid4)
     title = db.Column(db.String(100), primary_key=False)
     description = db.Column(db.Text(), primary_key=False)
 
-    def __init__(self, pk, uuid, title, description):
-        """ Create a new User """
-        self.pk = pk
+    def __init__(self, id, uuid, title, description):
+        """ Create a ad """
+        self.id = id
         self.uuid = uuid
         self.title = title
         self.description = description
