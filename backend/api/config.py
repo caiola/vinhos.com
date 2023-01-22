5704752bc25f1e48523d3dd97d26342ff4e6093c
Sérgio den Boer <den.boer.sergio@gmail.com>
2023-01-22 11:05:40 +0000

    Remove prefix `APPLICATION`from env variables

--- backend/api/config.py
+++ backend/api/config.py
@@ -10,16 +10,15 @@ PORT = int(os.getenv("APPLICATION_PORT", "3000"))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 MYSQL = {
-    "user": os.getenv("APPLICATION_MYSQL_USER", "mysql"),
-    "pw": os.getenv("APPLICATION_MYSQL_PW", ""),
-    "host": os.getenv("APPLICATION_MYSQL_HOST", "dbdefault"),
-    "port": os.getenv("APPLICATION_MYSQL_PORT", 5432),
-    "db": os.getenv("APPLICATION_MYSQL_DB", "mysql"),
+    "user": os.environ["MYSQL_USER"],
+    "pw": os.environ["MYSQL_PASSWORD"],
+    "host": os.environ["MYSQL_HOST"],
+    "port": os.environ["MYSQL_PORT"],
+    "db": os.environ["MYSQL_DATABASE"]
 }
 DB_URI = "mysql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s" % MYSQL
 
 logging.basicConfig(
-#   filename=os.getenv("SERVICE_LOG", "server.log"),
     level=logging.DEBUG,
     format="%(levelname)s: %(asctime)s \
         pid:%(process)s module:%(module)s %(message)s",
