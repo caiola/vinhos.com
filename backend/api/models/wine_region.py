40298de42eeb31aae7d396412bda17f5295b282e
Roberto Caiola <robccsilva@gmail.com>
2023-05-31 17:16:25 +0100

    DVL - Change region data type

--- backend/api/models/wine_region.py
+++ backend/api/models/wine_region.py
@@ -14,5 +14,5 @@ class WineRegion(db.Model, BaseModel, metaclass=MetaBaseModel):
                       Database.COLLATION_KEY: Database.COLLATION_VALUE}
 
     id = db.Column(db.BigInteger(), primary_key=True, nullable=False, comment="Primary key")
-    country = db.Column(db.BigInteger(), nullable=False, comment="Country")
+    country = db.Column(db.String(2), nullable=False, comment="Country")
     name = db.Column(db.String(50), nullable=True, comment="Name of region")
