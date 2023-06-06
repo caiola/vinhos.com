3c5c314560542594057de6527e9241cd3aa8cb55
Sérgio den Boer <den.boer.sergio@gmail.com>
2023-06-06 22:31:10 +0100

    Remove `manage.py` file
    Use `flask db` command instead

--- makefiles/database.mk
+++ makefiles/database.mk
@@ -1,15 +1,13 @@
 ### DATABASE
 # ¯¯¯¯¯¯¯¯
-
-
 database.connect: ## Connect to database
 	docker-compose -f docker-compse.deploy.yml exec db psql -Upostgres
 
 database.migrate: ## Create alembic migration file
-	docker-compose -f docker-compse.deploy.yml run --rm apiserver python manage.py db migrate
+	docker-compose -f docker-compse.deploy.yml run --rm apiserver flask db migrate
 
 database.upgrade: ## Upgrade to latest migration
-	docker-compose -f docker-compse.deploy.yml run --rm apiserver python manage.py db upgrade
+	docker-compose -f docker-compse.deploy.yml run --rm apiserver flask db upgrade
 
 database.downgrade: ## Downgrade latest migration
-	docker-compose -f docker-compse.deploy.yml run --rm apiserver python manage.py db downgrade
+	docker-compose -f docker-compse.deploy.yml run --rm apiserver flask db downgrade
